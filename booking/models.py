6935c1ffc59308268cf64b41804530f7ead12719
matzhedman <>
2023-05-15 15:31:58 +0000

    Add first model

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Record(models.Model):
+    name = models.CharField(max_length=50, null=False, blank=False)
+    phone = models.CharField(max_length=50, null=False, blank=False)
