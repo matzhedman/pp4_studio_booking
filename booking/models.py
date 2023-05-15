287dc497947e5f921bfb134c8b7d8ef90bafd953
matzhedman <>
2023-05-15 15:36:58 +0000

    Add self.name to class Record to display name input

--- booking/models.py
+++ booking/models.py
@@ -6,3 +6,6 @@ from django.db import models
 class Record(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False)
     phone = models.CharField(max_length=50, null=False, blank=False)
+
+    def __str__(self):
+        return self.name
